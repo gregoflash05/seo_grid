5004fdfbdd46d964cb0e0e32ae4e01a6635de1e3
believemanasseh <believemanasseh@gmail.com>
2020-08-07 02:59:20 +0100

    modified fields

--- api/models.py
+++ api/models.py
@@ -8,7 +8,33 @@ class Campaign(models.Model):
 	country = models.CharField(max_length=255, blank=True, null=True)
 
 	def __str__(self):
-		return self.user.name
+		return self.user.fullname
+
+class Site(models.Model):
+	est_traffic = models.FloatField(blank=True, null=True)
+	avg_position = models.FloatField(blank=True, null=True)
+	backlinks = models.FloatField(blank=True, null=True)
+	campaign = models.ManyToManyField(Campaign)
+
+	def __str__(self):
+		return self.campaign.link
+
+class Keywords(models.Model):
+	campaign = models.ManyToManyField(Campaign)
+	site = models.ManyToManyField(Site)
+	keyword = models.CharField(max_length=1000000000, blank=True, null=True)
+
+class KeywordDetails(models.Model):
+	ranking = models.IntegerField()
+	top_rank = models.IntegerField()
+	trend = models.ImageField(upload_to='trend/')
+	competitor_one = models.CharField(max_length=1000, blank=True, null=True)
+	competitor_two = models.CharField(max_length=1000, blank=True, null=True)
+	details = models.ManyToManyField(Keywords)
+
+
+
+
 
 
 
