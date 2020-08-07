141cdc05b39ece5cf79cf1bcdf776112e1b91ba1
believemanasseh <believemanasseh@gmail.com>
2020-08-07 01:41:50 +0100

    changed model field to a many-to-one relationship

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Campaign(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 	link = models.CharField(max_length=255, blank=False, null=True)
 	language = models.CharField(max_length=255, blank=True, null=True)
 	country = models.CharField(max_length=255, blank=True, null=True)
