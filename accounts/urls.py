72e7233a05b8d5272c3726f0815b6bb711a4c19b
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2020-08-14 18:53:10 +0100

    landing page integration

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.homepage, name = "homepage"),
     path('login/', views.login, name = "login"),
     path('logout/', views.logout, name="logout"),
-    path('dashboard/', views.dashboard, name="logout"),
+    # path('dashboard/', views.dashboard, name="logout"),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), name="password_reset_done"),
     path('reset_form/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/reset_form.html"), name="password_reset_confirm"),
