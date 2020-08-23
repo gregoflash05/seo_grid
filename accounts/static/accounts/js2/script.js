525db8db7a273db802b0c4fd06de6ef0dcf08808
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2020-08-23 12:09:28 +0100

    fix

--- accounts/static/accounts/js2/script.js
+++
@@ -1,8 +0,0 @@
-var deleteFaq = document.getElementsByClassName('text-red');
-var faqEntry = document.getElementsByClassName('faq-content');
-
-deleteFaq.addEventListener('click', remove());
-
-function remove() {
-    faqEntry.remove();
-}
